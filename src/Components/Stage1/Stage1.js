88953a217daf6e5fa97775277d0afcd6157c1c95
Manish Chaurasiya <manishchrsya@gmail.com>
2020-11-15 12:53:59 +0530

    csv uploader added

--- src/Components/Stage1/Stage1.js
+++ src/Components/Stage1/Stage1.js
@@ -1,16 +1,22 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import CSVReader from 'react-csv-reader';
+import './Stage1.css';
 
 const Stage1 = (props) => {
     return ( 
         <div className="text-center">
-            <Button
+            {/* <Button
             style={{margin:20, padding:20, fontSize:15}}
             variant="contained"
             color="primary"
             >
             Upload as CSV
-            </Button>
+            </Button> */}
+            <div>
+               <CSVReader inputStyle={{color: 'red'}}  cssInputClass='csv-reader' onFileLoaded={(data, fileInfo) => console.log(data, fileInfo)}/> 
+            </div>
+             
             <Button 
             style={{margin:20, padding:20, fontSize:15}}
             variant="contained"
